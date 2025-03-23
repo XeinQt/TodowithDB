66f05731a2df93c334e51b0dab010e89dc78bd09
My Name <myemail@example.com>
2025-03-24 07:25:33 +0800

    adding front-end

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div className="bg-amber-200 h-[100vh] flex items-center justify-center">
+      <div className="bg-red-500 ">
+        <div>
+          <input type="text" />
+          <button>Add</button>
+        </div>
+        <div>
+          <p>
+            e quae nulla minima. Necessitatibus rerum plum porro ab quisquam!
+          </p>
+          <div>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
