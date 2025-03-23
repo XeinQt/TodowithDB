702b28068eab2b63e353c3d8f8b71d548679c33d
My Name <myemail@example.com>
2025-03-24 07:41:53 +0800

    adding desgin in front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,47 @@
 function App() {
   return (
     <div className="bg-amber-200 h-[100vh] flex items-center justify-center">
-      <div className="bg-red-500 ">
-        <div>
-          <input type="text" />
-          <button>Add</button>
+      <div className="bg-blue-400 rounded-3xl w-2xl">
+        <h1 className="text-center text-5xl font-bold mt-5 mb-5">Todo List</h1>
+        {/* top div */}
+        <div className=" py-3 px-2 rounded-t-3xl">
+          <input
+            className="bg-white w-3/4 px-2 py-3 focus: outline-none"
+            type="text"
+          />
+          <button className="bg-green-500 w-1/4 px-2 py-3 text-white cursor-pointer">
+            Add
+          </button>
         </div>
-        <div>
-          <p>
-            e quae nulla minima. Necessitatibus rerum plum porro ab quisquam!
-          </p>
-          <div>
-            <button>Edit</button>
-            <button>Delete</button>
+        {/* bottom div */}
+        <div className="bg-red-300 py-5 px-2 rounded-b-3xl">
+          {/*1st todo list*/}
+          <div className="bg-white flex mb-3">
+            {" "}
+            <p className="w-3/4 py-3 px-2 ">Doign Home work</p>
+            <div className="w-1/4">
+              <button className="py-3 px-2 bg-yellow-300 w-1/2 cursor-pointer">
+                Edit
+              </button>
+              <button className="py-3 px-2 bg-red-400 w-1/2 cursor-pointer">
+                Delete
+              </button>
+            </div>
           </div>
+          {/*wnd todo list*/}
+          <div className="bg-white flex ">
+            {" "}
+            <p className="w-3/4 py-3 px-2 ">Doign Home work</p>
+            <div className="w-1/4">
+              <button className="py-3 px-2 bg-yellow-300 w-1/2 cursor-pointer">
+                Edit
+              </button>
+              <button className="py-3 px-2 bg-red-400 w-1/2 cursor-pointer">
+                Delete
+              </button>
+            </div>
+          </div>
+          {/*End */}
         </div>
       </div>
     </div>
